6baaa30b39f129e0aa39b25d00244d7491a9eb58
asav-de <andrii_savchenko@teilnehmer.cdemy.de>
2026-08-31 17:17:25 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Counter App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _dicrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _incrementCounter2() {
+    setState(() {
+      _counter += 2;
+    });
+  }
+
+  void _divideCounter() {
+    setState(() {
+      _counter = _counter ~/ 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.plus_one),
+          ),
+          FloatingActionButton(
+            onPressed: _incrementCounter2,
+            tooltip: 'Increment by 2',
+            child: const Icon(Icons.exposure_plus_2),
+          ),
+          FloatingActionButton(
+            onPressed: _divideCounter,
+            tooltip: 'Divide',
+            child: const FaIcon(FontAwesomeIcons.divide),
+          ),
+          FloatingActionButton(
+            onPressed: _dicrementCounter,
+            tooltip: 'Dicrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
