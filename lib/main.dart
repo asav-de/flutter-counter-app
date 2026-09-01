cbaec34a32d4a31d5c89e0ee888653499419a3bf
asav-de <andrii_savchenko@teilnehmer.cdemy.de>
2026-09-01 11:25:37 +0200

    Wrap counter in styled container, added action Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    return addActionButton(context);
+  }
+
+  Scaffold addActionButton(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -67,9 +71,19 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: .center,
           children: [
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
             ),
           ],
         ),
